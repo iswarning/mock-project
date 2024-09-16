56b610c9427bee3802a31583ca30a721ed66edc6
YenLinh <nvyenlinh05@gmail.com>
2024-09-16 20:55:02 +0700

    update source code

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -4,37 +4,57 @@ import "./style.css";
 const Login = () => {
   return (
     <div className="containerLogin">
-      <div className="main">
-        <input type="checkbox" id="chk" aria-hidden="true" />
+      <div className="mainLogin">
+        <input
+          className="inputLogin"
+          type="checkbox"
+          id="chk"
+          aria-hidden="true"
+        />
         <div className="login">
           <form>
-            <label htmlFor="chk" aria-hidden="true">
+            <label className="labelLogin" htmlFor="chk" aria-hidden="true">
               Login
             </label>
-            <input type="email" name="email" placeholder="Email" required="" />
             <input
+              className="inputLogin"
+              type="email"
+              name="email"
+              placeholder="Email"
+            />
+            <input
+              className="inputLogin"
               type="password"
               name="pswd"
               placeholder="Password"
-              required=""
             />
-            <button>Login</button>
+            <button className="btnLogin">Login</button>
           </form>
         </div>
         <div className="signUp">
           <form>
-            <label htmlFor="chk" aria-hidden="true">
+            <label className="labelLogin" htmlFor="chk" aria-hidden="true">
               Sign up
             </label>
-            <input type="text" name="txt" placeholder="User name" required="" />
-            <input type="email" name="email" placeholder="Email" required="" />
             <input
+              className="inputLogin"
+              type="text"
+              name="txt"
+              placeholder="User name"
+            />
+            <input
+              className="inputLogin"
+              type="email"
+              name="email"
+              placeholder="Email"
+            />
+            <input
+              className="inputLogin"
               type="password"
               name="pswd"
               placeholder="Password"
-              required=""
             />
-            <button>Sign up</button>
+            <button className="btnLogin">Sign up</button>
           </form>
         </div>
       </div>
--- src/store/constants.js
+++ src/store/constants.js
@@ -1,1 +1,6 @@
 export const baseUrl = "http://weblearn.ddns.net:4001/api/";
+
+export const TOAST = {
+  SUCCESS: "SUCCESS",
+  ERROR: "ERROR",
+};
