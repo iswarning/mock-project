c104c03982eefcb6b9871e025dba0ff13153e999
YenLinh <nvyenlinh05@gmail.com>
2024-09-23 19:20:10 +0700

    update source code

--- src/pages/users/RoleUser.jsx
+++ src/pages/users/RoleUser.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import "./style.scss";
 
 function RoleUser(props) {
+  const { userInfo } = useSelector((state) => state.authStore);
+
   return (
-    <div className="row">
-      <div className="col-8">info</div>
-      <div className="col-4">avatar</div>
+    <div className="infoUser">
+      <h2>General</h2>
+      <h2>Password</h2>
     </div>
   );
 }
