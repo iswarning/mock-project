6239fc439fce7ad6222cc7b1e7278c44943207b8
YenLinh <nvyenlinh05@gmail.com>
2024-10-04 10:42:11 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { ROUTES } from './common/constants';
-import ProtectedRoute from './components/ProtectedRoute';
-import Dashboard from './pages/dashboard/Dashboard';
-import Home from './pages/home/Home';
-import WrapperLogin from './pages/login/WrapperLogin';
-import Tasks from './pages/tasks/Tasks';
-import Users from './pages/users/WrapperUsers';
-import WrapperProjects from './pages/projects/WrapperProjects';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ROUTES } from "./common/constants";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Home from "./pages/home/Home";
+import WrapperLogin from "./pages/login/WrapperLogin";
+import Tasks from "./pages/tasks/Tasks";
+import Users from "./pages/users/WrapperUsers";
+import WrapperProjects from "./pages/projects/WrapperProjects";
 
 function App() {
   return (
