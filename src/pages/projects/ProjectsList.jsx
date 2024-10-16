a426c062246defcac3def52d3d5342ca876e9617
ThuanLT <lethuan.it.252@gmail.com>
2024-10-16 18:44:25 +0700

    Change

--- src/pages/projects/ProjectsList.jsx
+++ src/pages/projects/ProjectsList.jsx
@@ -1,56 +1,30 @@
-import { debounce } from "lodash";
-import React, { useCallback, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { convertDateToDMY } from "../../common/dateFormat";
-import projectsPagination from "../../hooks/projectsPagination";
-import { deleteProject } from "../../store/actions/projectAction";
-import { SET_CURRENT_PAGE } from "../../store/constants";
-import ProjectCreateModal from "./ProjectCreateModal";
-import ProjectUpdateModal from "./ProjectUpdateModal";
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { convertDateToDMY } from '../../common/dateFormat';
+import { deleteProject } from '../../store/actions/projectAction';
+import ProjectCreateModal from './ProjectCreateModal';
+import ProjectUpdateModal from './ProjectUpdateModal';
 
 const ProjectsList = ({ projects }) => {
+  console.log('🚀 ~ projects:', projects);
   const [projectData, setProjectData] = useState({
-    name: "",
-    payment: "",
-    time_start: "",
-    time_end: "",
-    note: "",
-    priority: "",
+    name: '',
+    payment: '',
+    time_start: '',
+    time_end: '',
+    note: '',
+    priority: '',
   });
-  const [text, setText] = useState("");
-  const { currentPage } = useSelector((state) => state.projectStore);
   const { userInfo } = useSelector((state) => state.authStore);
   const dispatch = useDispatch();
-  const { totalPage, paginatedData } = projectsPagination(projects, text);
 
   const handleDelete = (id) => {
-    if (confirm("Are you sure delete this project?"))
-      dispatch(deleteProject({ id }));
+    if (confirm('Are you sure delete this project?')) dispatch(deleteProject({ id }));
   };
 
   const showProjectUpdateModal = (project) => {
     setProjectData(project);
   };
-
-  const handleChangePage = (page) => {
-    dispatch({
-      type: SET_CURRENT_PAGE,
-      payload: page,
-    });
-  };
-
-  const handleSetText = useCallback(
-    debounce(
-      (value) => {
-        setText(value);
-        dispatch({
-          type: SET_CURRENT_PAGE,
-          payload: 1,
-        });
-      },
-      [1000]
-    )
-  );
 
   return (
     <>
@@ -87,19 +61,16 @@ const ProjectsList = ({ projects }) => {
       </div>
       <div className="container my-4">
         <div className="row">
-          {paginatedData["page" + currentPage] &&
-            paginatedData["page" + currentPage].map((project) => (
-              <div
-                className="col-xl-3 col-lg-6 col-md-6 col-sm-6 mb-4"
-                key={project.id}
-              >
+          {projects &&
+            projects.map((project) => (
+              <div className="col-xl-3 col-lg-6 col-md-6 col-sm-6 mb-4" key={project.id}>
                 <div
                   className={`card bg-light shadow-sm border border-3 ${
                     project.priority === 1
-                      ? "border-danger"
+                      ? 'border-danger'
                       : project.priority === 2
-                      ? "border-warning"
-                      : ""
+                      ? 'border-warning'
+                      : ''
                   }`}
                 >
                   <div className="card-body">
@@ -152,61 +123,6 @@ const ProjectsList = ({ projects }) => {
             ))}
         </div>
       </div>
-      {totalPage > 1 && (
-        <div className="col-md-12">
-          <nav aria-label="Page navigation example">
-            <ul className="pagination justify-content-center">
-              <li
-                className={`page-item ${currentPage === 1 ? "disabled" : ""}`}
-              >
-                <a
-                  role="button"
-                  className="page-link"
-                  onClick={() => handleChangePage(currentPage - 1)}
-                >
-                  Previous
-                </a>
-              </li>
-              {Array.from({ length: totalPage }).map((p, i) =>
-                i + 1 === currentPage ? (
-                  <li key={i} className="page-item active" aria-current="page">
-                    <span
-                      role="button"
-                      className="page-link"
-                      onClick={() => handleChangePage(i + 1)}
-                    >
-                      {i + 1}
-                    </span>
-                  </li>
-                ) : (
-                  <li key={i} className="page-item">
-                    <a
-                      className="page-link"
-                      role="button"
-                      onClick={() => handleChangePage(i + 1)}
-                    >
-                      {i + 1}
-                    </a>
-                  </li>
-                )
-              )}
-              <li
-                className={`page-item ${
-                  currentPage === totalPage ? "disabled" : ""
-                }`}
-              >
-                <a
-                  role="button"
-                  className="page-link"
-                  onClick={() => handleChangePage(currentPage + 1)}
-                >
-                  Next
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      )}
       <ProjectCreateModal />
       <ProjectUpdateModal projectData={projectData} />
     </>
