b71291cdc79bd471e28c119c7b223199b9a3e722
ThuanLT <lethuan.it.252@gmail.com>
2024-10-30 20:33:48 +0700

    Done Projects Charts

--- src/components/TaskChart.jsx
+++ src/components/TaskChart.jsx
@@ -1,65 +1,70 @@
 // BarChart.js
 import React, { useMemo } from 'react';
 import { Line } from 'react-chartjs-2';
-import { Chart, registerables } from 'chart.js'
+import { Chart, registerables } from 'chart.js';
 import { useSelector } from 'react-redux';
 
-Chart.register(...registerables)
+Chart.register(...registerables);
 
-const monthsCharacter = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+const monthsCharacter = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+];
 
 const TaskChart = () => {
-  
-  const { listTask } = useSelector((state) => state.taskStore)
+  const { listTask } = useSelector((state) => state.taskStore);
 
   const filteredTasks = useMemo(() => {
-    let labels = []
-    const currentDate = new Date(Date.now()).getDate()
+    let labels = [];
+    const currentDate = new Date(Date.now()).getDate();
 
-    const monthOfTimeStart = listTask.map((task) => new Date(task.time_start).getMonth())
-    const monthOfTimeEnd = listTask.map((task) => new Date(task.time_end).getMonth())
+    const monthOfTimeStart = listTask.map((task) => new Date(task.time_start).getMonth());
+    const monthOfTimeEnd = listTask.map((task) => new Date(task.time_end).getMonth());
 
-    const minMonth = Math.min(...monthOfTimeStart)
-    const maxMonth = Math.max(...monthOfTimeEnd)
+    const minMonth = Math.min(...monthOfTimeStart);
+    const maxMonth = Math.max(...monthOfTimeEnd);
 
     let tasks = {
       overdue: [],
       pending: [],
       inprogress: [],
-      completedNextThreeDay: []
-    }
+      completedNextThreeDay: [],
+    };
 
     for (let i = minMonth; i <= maxMonth; i++) {
-      labels.push(monthsCharacter[i])
+      labels.push(monthsCharacter[i]);
 
-      const totalTasksOfMonth = listTask
-      .filter((task) => new Date(task.time_end).getMonth() === i)
+      const totalTasksOfMonth = listTask.filter((task) => new Date(task.time_end).getMonth() === i);
 
-      tasks.overdue.push(totalTasksOfMonth
-        .filter((task) => currentDate > (new Date(task.time_end).getDate()))
-        .length
-      )
+      tasks.overdue.push(
+        totalTasksOfMonth.filter((task) => currentDate > new Date(task.time_end).getDate()).length
+      );
 
-      tasks.pending.push(totalTasksOfMonth
-       .filter((task) => task.status === 1)
-       .length
-      )
+      tasks.pending.push(totalTasksOfMonth.filter((task) => task.status === 1).length);
 
-      tasks.inprogress.push(totalTasksOfMonth
-       .filter((task) => task.status === 2)
-       .length
-      )
+      tasks.inprogress.push(totalTasksOfMonth.filter((task) => task.status === 2).length);
 
-      tasks.completedNextThreeDay.push(totalTasksOfMonth
-        .filter((task) => task.status === 3 && (new Date(task.time_end).getDate()) - currentDate === 3)
-       .length
-      )
+      tasks.completedNextThreeDay.push(
+        totalTasksOfMonth.filter(
+          (task) => task.status === 3 && new Date(task.time_end).getDate() - currentDate === 3
+        ).length
+      );
     }
     return {
       ...tasks,
-      labels
-    }
-  },[listTask])
+      labels,
+    };
+  }, [listTask]);
 
   const data = {
     labels: filteredTasks.labels,
---
+++ src/pages/projects/ProjectsChart.jsx
@@ -0,0 +1,115 @@
+// BarChart.js
+import { Chart, registerables } from 'chart.js';
+import React, { useMemo } from 'react';
+import { Line } from 'react-chartjs-2';
+import { useSelector } from 'react-redux';
+
+Chart.register(...registerables);
+
+const monthsCharacter = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+];
+
+const ProjectsChart = () => {
+  const { projects } = useSelector((state) => state.projectStore);
+
+  const filterProjects = useMemo(() => {
+    let labels = [];
+    const currentDate = new Date(Date.now()).getDate();
+
+    const monthOfTimeStart = projects.map((project) => new Date(project.time_start).getMonth());
+    const monthOfTimeEnd = projects.map((project) => new Date(project.time_end).getMonth());
+
+    const minMonth = Math.min(...monthOfTimeStart);
+    const maxMonth = Math.max(...monthOfTimeEnd);
+
+    let project = {
+      inprogress: [],
+      completedNextSevenDay: [],
+      priority: [],
+    };
+
+    console.log(project.completedNextSevenDay);
+
+    for (let i = minMonth; i <= maxMonth; i++) {
+      labels.push(monthsCharacter[i]);
+
+      const totalTasksOfMonth = projects.filter(
+        (project) => new Date(project.time_end).getMonth() == i
+      );
+
+      project.inprogress.push(
+        totalTasksOfMonth.filter((project) => currentDate >= new Date(project.time_start).getDate())
+          .length
+      );
+
+      project.completedNextSevenDay.push(
+        totalTasksOfMonth.filter((project) => {
+          const differenceInMillis = new Date(project.time_end) - new Date();
+          const differenceInDays = differenceInMillis / (1000 * 60 * 60 * 24);
+          return differenceInDays <= 7;
+        }).length
+      );
+
+      project.priority.push(totalTasksOfMonth.filter((project) => project.priority === 1).length);
+    }
+    return {
+      ...project,
+      labels,
+    };
+  }, [projects]);
+
+  const data = {
+    labels: filterProjects.labels,
+    datasets: [
+      {
+        label: 'In Progress',
+        data: filterProjects.inprogress,
+        fill: false,
+        borderColor: 'rgba(0, 0, 255, 0.5)',
+        tension: 0.4,
+      },
+      {
+        label: 'Completed in next seven days',
+        data: filterProjects.completedNextSevenDay,
+        fill: false,
+        borderColor: 'rgba(255, 255, 0, 0.5)',
+        tension: 0.4,
+      },
+      {
+        label: 'Priority',
+        data: filterProjects.priority,
+        fill: false,
+        borderColor: 'rgba(255, 0, 0, 0.5)',
+        tension: 0.4,
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+    elements: {
+      line: {
+        tension: 0.4,
+      },
+    },
+  };
+  return <Line data={data} options={options} />;
+};
+
+export default ProjectsChart;
